e74b1ff7ad68f4427f29f36b58f260893856cfb7
Ben Frederickson <github@benfrederickson.com>
2024-10-15 16:38:55 -0700

    remove config.mak.32 references
    mirror logic in https://github.com/rust-cross/rust-musl-cross/commit/67d7cb13c8fe971787fe216e742477ac4b2a8381

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ docker build -t benfred/rust-musl-cross:x86_64-musl .
 # aarch64-unknown-linux-musl
 docker build --build-arg TARGET=aarch64-unknown-linux-musl --build-arg RUST_MUSL_MAKE_CONFIG=config.mak -t benfred/rust-musl-cross:aarch64-musl .
 # armv7-unknown-linux-musleabihf
-docker build --build-arg TARGET=armv7-unknown-linux-musleabihf --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t benfred/rust-musl-cross:armv7-musleabihf .
+docker build --build-arg TARGET=armv7-unknown-linux-musleabihf --build-arg RUST_MUSL_MAKE_CONFIG=config.mak -t benfred/rust-musl-cross:armv7-musleabihf .
 # i686-unknown-linux-musl
-docker build --build-arg TARGET=i686-unknown-linux-musl --build-arg RUST_MUSL_MAKE_CONFIG=config.mak.32 -t benfred/rust-musl-cross:i686-musl .
+docker build --build-arg TARGET=i686-unknown-linux-musl --build-arg RUST_MUSL_MAKE_CONFIG=config.mak -t benfred/rust-musl-cross:i686-musl .
 # powerpc64le-unknown-linux-musl
 docker build --build-arg TARGET=powerpc64le-unknown-linux-musl --build-arg RUST_MUSL_MAKE_CONFIG=config.mak --build-arg TOOLCHAIN=nightly -t benfred/rust-musl-cross:powerpc64le-musl .
